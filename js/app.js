2f812ded68317e1f3bc6017c396906832f944f75
Unknown <diegopm07@gmail.com>
2017-09-12 03:37:59 -0300

    VELHO EU TO MUITO PUTO

---
+++ js/app.js
@@ -0,0 +1,55 @@
+"use strict";
+$(document).ready(function() {
+    /*function loadJSON(file,callback) {        
+        var xobj = new XMLHttpRequest();
+        var area;
+        xobj.overrideMimeType("text/plain");
+        xobj.open('GET', 'json/'+file, true);
+        xobj.onreadystatechange = function() {
+        if (xobj.readyState == 4 && xobj.status == "200") {        
+        // .open will NOT return a value but simply returns undefined in async mode so use a callback
+            callback( xobj.responseText);   
+                         
+        }
+        }
+        xobj.send(null);        
+    }
+    var II;    
+
+    loadJSON('II.json',function(response){
+            console.log(response);
+
+    }); ESSA FUNCAO QUANDO TENTA EXECUTAR COMO AJAX É ASSINCRONO MANTEM APENAS DENTOR DA FUNCAO
+    console.log(II);
+
+    $.getJSON( "json/II.json", function( data ) {
+       console.log(data);  MESMO CASO POIS UTILIZA AJAX É ASSINCRONO
+    
+    
+    });
+    var II = require('./json/II.json'); AQUI TENTEI USAR REQUIRE JS MAS FALHEI
+    console.log(II);
+   
+    $.ajax({
+        url: 'json/II.json',
+        dataType: 'application/json',
+        complete: function(data){
+            console.log(data);
+        }
+            
+    });    
+        */ 
+        
+    
+   
+    
+   
+
+    
+
+
+
+
+
+
+});
